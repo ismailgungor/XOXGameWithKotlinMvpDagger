ce4bf18b60a2708112ab9b15cc1487e4e7617996
ismailgungor <ismail.gungor.92@gmail.com>
2018-01-13 15:22:38 +0300

    Created Main Activity class and designed activity_main layout

---
+++ app/src/main/java/com/example/xoxgamewithkotlinmvpdagger/mainActivity/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.xoxgamewithkotlinmvpdagger.mainActivity
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by ismailgungor on 13.01.2018.
+ */
+class MainActivity: AppCompatActivity() {
+}
